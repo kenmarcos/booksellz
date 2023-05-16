e821d0734917c3431c933faf0103fc4dab67d378
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-05-16 14:20:03 -0300

    feat: create layut of releases page

---
+++ src/pages/books/releases/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import * as M from "@mui/material";
+
+export const Wrapper = styled(M.Box)`
+  padding-top: ${({ theme }) => theme.spacing(5)};
+  padding-bottom: ${({ theme }) => theme.spacing(5)};
+`;
+
+export const BooksSection = styled(M.Box)``;
