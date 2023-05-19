b8d0cb1f85bd7752f99146ed8a29e2e96e5844d5
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-05-19 15:47:59 -0300

    fix: add adjustments in styled components

--- src/pages/books/releases/index.tsx
+++ src/pages/books/releases/index.tsx
@@ -1,47 +1,14 @@
-import Image from "next/image";
 import { GetStaticProps } from "next";
 
-import * as M from "@mui/material";
-
-import * as S from "./styles";
-import BookCard from "@/components/BookCard";
-import bannerReleases from "/public/images/banner-releases.png";
+import ReleasesTemplate, {
+  ReleasesTemplateProps,
+} from "@/templates/books/releases";
 import { api } from "@/services/api";
-import { Book } from "@/types/books";
 
-interface ReleaseProps {
-  bookReleases: Book[];
-}
+type ReleaseProps = ReleasesTemplateProps;
 
 const Releases = ({ bookReleases }: ReleaseProps) => {
-  return (
-    <S.Wrapper>
-      <S.Banner>
-        <Image
-          src={bannerReleases}
-          alt="banner releases"
-          quality={100}
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOUlJauBwABdADQDG2nxgAAAABJRU5ErkJggg=="
-        />
-      </S.Banner>
-
-      <M.Box>
-        <M.Grid container spacing={2}>
-          {bookReleases.map((book) => (
-            <M.Grid key={book.isbn13} item xs={12} sm={6} md={3}>
-              <BookCard
-                isbn13={book.isbn13}
-                image={book.image}
-                title={book.title}
-                price={book.price}
-              />
-            </M.Grid>
-          ))}
-        </M.Grid>
-      </M.Box>
-    </S.Wrapper>
-  );
+  return <ReleasesTemplate bookReleases={bookReleases} />;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
--- src/pages/order/confirmation/index.tsx
+++ src/pages/order/confirmation/index.tsx
@@ -1,42 +1,7 @@
-import Link from "next/link";
-
-import * as M from "@mui/material";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-
-import * as S from "./styles";
+import OrderConfirmationTemplate from "@/templates/order/confirmation";
 
 const OrderConfirmation = () => {
-  return (
-    <S.Wrapper>
-      <S.MessageBox component="section">
-        <M.Paper>
-          <CheckCircleIcon color="success" />
-
-          <M.Box textAlign="center">
-            <M.Typography variant="h2">Your Order is Complete!</M.Typography>
-            <M.Typography variant="h4">
-              Thank you for your purchase.
-            </M.Typography>
-          </M.Box>
-
-          <M.Typography textAlign="center" variant="h5">
-            You will be receiving a confirmation e-mail with order details.
-          </M.Typography>
-
-          <M.Box>
-            <M.Button
-              LinkComponent={Link}
-              href="/"
-              variant="contained"
-              size="large"
-            >
-              Continue Shopping
-            </M.Button>
-          </M.Box>
-        </M.Paper>
-      </S.MessageBox>
-    </S.Wrapper>
-  );
+  return <OrderConfirmationTemplate />;
 };
 
 export default OrderConfirmation;
